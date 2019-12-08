49d9714cb30014b8d7d772c9116bfa02d0b30c41
Paul <Semprini@users.noreply.github.com>
2019-12-09 12:01:07 +1300

    Update image_update.py

--- mdg/confluence/image_update.py
+++ mdg/confluence/image_update.py
@@ -1,11 +1,12 @@
 import os
 import glob
 
-from xmi.confluence.util import basic_auth, http_post, http_get_json
+from mdg.confluence.util import basic_auth, http_post, http_get_json
 
+ORG = "my org"
 
 def update_images(auth, content_id, path):
-    attachments, session = http_get_json("https://jiragenesisenergy.atlassian.net/wiki/rest/api/content/{}/child/attachment".format(content_id),auth)
+    attachments, session = http_get_json("https://{}.atlassian.net/wiki/rest/api/content/{}/child/attachment".format(ORG, content_id),auth)
 
     files = [f for f in glob.glob(path + "/*.png", recursive=False)]
 
@@ -20,14 +21,14 @@ def update_images(auth, content_id, path):
         if conf_attachment is not None:
             # Update if different file size
             if os.path.getsize(file) != conf_attachment['extensions']['fileSize']:
-                url = "https://jiragenesisenergy.atlassian.net/wiki/rest/api/content/{}/child/attachment/{}/data".format( content_id, attachment['id'] )
+                url = "https://{}.atlassian.net/wiki/rest/api/content/{}/child/attachment/{}/data".format( ORG, content_id, attachment['id'] )
                 http_post( url, auth, None, file, None )
                 print("Uploaded local file {} to confluence".format(file))
             else:
                 print("Skipping local file {} as same file size detected in confluence".format(file))
         else:
             # TODO: Upload new attachment
-            url = "https://jiragenesisenergy.atlassian.net/wiki/rest/api/content/{}/child/attachment/".format( content_id, attachment['id'] )
+            url = "https://{}.atlassian.net/wiki/rest/api/content/{}/child/attachment/".format( ORG, content_id, attachment['id'] )
             http_post( url, auth, None, file, None )
             print("Uploaded new local file {} to confluence".format(file))
 
