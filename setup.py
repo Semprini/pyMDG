ebd6e230f84d9b1d0fccb610b6ec8823dde63ab0
Semprini <atkinpa@gmail.com>
2021-03-08 22:26:21 +1300

    Version bump

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='pymdg',
-    version='0.4a3',
+    version='0.4a4',
     author='Semprini',
     author_email='dont@contact.me',
     description='Model driven genration - from UML to Code & Docs',
