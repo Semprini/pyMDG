766df1d36eb2817b78a63d563b69cc62b42c9a23
Semprini <atkinpa@gmail.com>
2024-04-27 23:01:59 +1200

    Version 1.1.0

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='pymdg',
-    version='1.0.0-alpha',
+    version='1.1.0',
     author='Semprini',
     author_email='dont@contact.me',
     description='Model driven genration - from UML to Code & Docs',
