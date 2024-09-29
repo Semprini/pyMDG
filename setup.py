c083e306aab56caf305d21ebf04f704ad00c468b
Semprini <atkinpa@gmail.com>
2024-09-30 10:49:33 +1300

    Updated drawio parse

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='pymdg',
-    version='1.2.4',
+    version='1.2.5',
     author='Semprini',
     author_email='dont@contact.me',
     description='Model driven genration - from UML to Code & Docs',
